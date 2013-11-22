5d80c38cec87f3ab74f8177ab57be679fc97876f
Ohjann <eoghan@hyn.es>
2013-11-22 20:25:03 +0000

    broken Load function but working consts and semi-working strings

--- compile.sh
+++ compile.sh
@@ -1,15 +1,4 @@
-T_STATUS=0
-OUTPUT=`mono bin/tcc.exe test/Programs/Test.TAS test.asm` || EXIT_STATUS=$?
-if [ ! -z "$OUTPUT" ]
-then
-    echo $OUTPUT
-    exit $EXIT_STATUS
-fi
-OUTPUT=`tasm test.asm test.bc` || EXIT_STATUS=$?
-if [ ! -z "$OUTPUT" ]
-then
-    echo $OUTPUT
-    exit $EXIT_STATUS
-fi
-tvm test.bc test/Inputs/test.IN || EXIT_STATUS=$?
-exit $EXIT_STATUS
+mono bin/tcc.exe test/Programs/Test.TAS test.asm
+printf "\n"
+tasm test.asm test.bc
+tvm test.bc test/Inputs/test.IN 
